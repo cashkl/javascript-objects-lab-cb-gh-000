50115ac786ab8bd9b12e3908098f99597d8e6e42
Murat Celik <cashkl@yandex.com>
2018-04-27 21:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var recipes=new Object()
 function updateObjectWithKeyAndValue(object, key,value) {
-  return Object.assign({},object,[key]:value)
+  return Object.assign({},object,{[key]:value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
-  return Object.assign(object,[key]:value)
+  return Object.assign(object,{key]:value})
 }
 function deleteFromObjectByKey(object,key) {
 var newObj=Object.assign({},object)
